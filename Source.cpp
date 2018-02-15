6c09d156168163ace2683863625f65396ea1ef36
Jason Lieu <jasonlieuthefirst@gmail.com>
2018-02-14 18:04:40 -0800

    functions for main.cpp

--- Source.cpp
+++ Source.cpp
@@ -6,93 +6,43 @@
 #include <algorithm>
 using namespace std;
 
-class Log {
-private:
-	fstream logFile;
-	string name = "";
-	time_t currentTimeDate;
-public:
-	Log() {}										//default constructor
-	Log(std::string name) :name(name) {}
-	~Log() {										//destructor
-		logFile.close();
+fstream openFile(string name) {
+	fstream temp;
+	temp.open(name);
+	return temp;
+};
+void writeLine(fstream &a,/* int lineNumber, string functionName,*/ string input) {
+	//a << lineNumber<<" "<< __TIME__<< " " << __DATE__ << " " << input <<endl;
+	a << __func__ << " " << __TIME__ << " " << __DATE__ << " " << input << endl;
+	fflush(stdout);
+};
+string read(fstream &a, int lineNumber) {
+	string line;
+	int currentLine = 0;
+	a.seekg(0, a.beg);					//jump to beginning
+	for (int i = 0; i < lineNumber; i++) {				//loop through lines until number
+		currentLine++;
+		getline(a, line);
 	}
-	void openExistingFile(string name) {			//opens existing file given file name
-		this->name = name;
-		try {
-			logFile.open(name);
-			if (!logFile) {
-				throw std::runtime_error("* " + name + " *CAN'T OPEN INFILE OR INFILE DOESN'T EXISTS");
-			}
-			else {
-				std::cout << "* " << name << " * INPUT MYLOG IS OPENED" << std::endl;
-			}
-		}
-		catch (std::runtime_error &e) {
-			std::cout << e.what() << std::endl;
-		}
-	}
-	void openNewFile(string name) {					//opens new file with name
-		logFile.open(name);
-	}
-	void readLine(int number) {						//print current line
-		string line;
-		int currentLine = 0;
-		logFile.seekg(0, logFile.beg);					//jump to beginning
-		for (int i = 0; i < number; i++) {				//loop through lines until number
-			currentLine++;
-			getline(logFile, line);
-		}
-		cout << currentLine << ": " << line << endl;
-	}
-	void readLines(int from, int to) {				//read lines from given numbers. Inclusive
-		string line;
-		int currentLine = 0;
-		logFile.seekg(0, logFile.beg);					//jump to beginning
-		for (int i = 0; i < from; i++) {				//loop through lines until number
-			currentLine++;
-			getline(logFile, line);
-		}
-		for (int i = currentLine; i <= to; i++) {		//loops from from to to, print lines
-			cout << currentLine << ": " << line << endl;
-			currentLine++;
-			getline(logFile, line);
-		}
-	}
-	void writeToFile(string input) {				//write input at the end of file
-		logFile.seekg(0, logFile.end);					//jump to end
-		logFile << setTime() << " " << input << endl;						//write
-	}
-	void flushLog() {
-		logFile.flush();
-	}
-	void closeMyLog() {
-		if (logFile.is_open()) {
-			logFile.close();
-			std::cout << "* " << name << " * LOGFILE IS CLOSED" << std::endl;
-		}
-		else {
-			std::cout << "* " << name << " * MYLOG DIDN'T NOT GET OPENED" << std::endl;
-		}
-	}
-	string setTime() {
-		time(&currentTimeDate);
-		char timeStr[26];
-		ctime_s(timeStr, sizeof timeStr, &currentTimeDate);
-		string timeTime(timeStr);
-		timeTime.erase(remove(timeTime.begin(), timeTime.end(), '\n'), timeTime.end());
-		return timeTime;
-	}
+	return line;
 };
 
-
 int main() {
-	Log *test = new Log();
-	time_t currentTimeDate;
-	time(&currentTimeDate);
-	test->openExistingFile("Text.txt");
+	fstream a = openFile("Text.txt");
+	writeLine(a, "1");
+	writeLine(a, "2");
+	writeLine(a, "3");
+	cout << read(a, 1) << endl;
+	cout << read(a, 2) << endl;
+	cout << read(a, 3) << endl;
 
-	test->writeToFile("TEST9");
+	int asd = __LINE__;
+	string d = __DATE__;
+	string t = __TIME__;
+	cout << endl << asd << endl << d << endl << t;
+
+
+
 
 
 	//test->readLine(5);
@@ -100,3 +50,18 @@ int main() {
 
 	system("pause");
 }
+
+
+
+/*
+class1.so
+open				can place classes into /usr/local/lib
+read				then include them in main program
+write
+close
+flush
+
+class2.so
+creates the lines that are passed into class1
+
+*/
